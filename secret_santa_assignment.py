4d4f784cda26f266e1fe7820a82c161ad1fae491
cshakthishankaran <chandranshakthishankaran@gmail.com>
2024-10-27 21:15:41 +0530

    exception handling

--- secret_santa_assignment.py
+++ secret_santa_assignment.py
@@ -14,26 +14,36 @@ class SecretSantaAssignment:
 
     def assign_secret_children(self):
         """Assign secret children while respecting all constraints."""
-        available_children = self.employees.copy()
-        random.shuffle(available_children)
+        try:
+            available_children = self.employees.copy()
+            random.shuffle(available_children)
 
-        for employee in self.employees:
-            for child in available_children:
-                if child.email != employee.email and child.email != self.previous_assignments.get(employee.email):
-                    employee.assign_secret_child(child)
-                    available_children.remove(child)
-                    break
-            else:
-                raise ValueError("Unable to complete assignments due to constraints.")
+            for employee in self.employees:
+                for child in available_children:
+                    if child.email != employee.email and child.email != self.previous_assignments.get(employee.email):
+                        employee.assign_secret_child(child)
+                        available_children.remove(child)
+                        break
+                else:
+                    raise ValueError("Unable to complete assignments due to constraints.")
 
-        if available_children:
-            raise ValueError("Some employees were not assigned Secret Santa children.")
+            if available_children:
+                raise ValueError("Some employees were not assigned Secret Santa children.")
 
+        except Exception as e:
+            print(str(e))
+            raise Exception(str(e))    
+        
     def get_assignments(self):
-        """Get a list of current assignments in dictionary format."""
-        return [{
-            "Employee_Name": e.name,
-            "Employee_EmailID": e.email,
-            "Secret_Child_Name": e.secret_child.name if e.secret_child else None,
-            "Secret_Child_EmailID": e.secret_child.email if e.secret_child else None
-        } for e in self.employees]
+        try:    
+            """Get a list of current assignments in dictionary format."""
+            return [{
+                "Employee_Name": e.name,
+                "Employee_EmailID": e.email,
+                "Secret_Child_Name": e.secret_child.name if e.secret_child else None,
+                "Secret_Child_EmailID": e.secret_child.email if e.secret_child else None
+            } for e in self.employees]
+
+        except Exception as e:
+            print(str(e))
+            raise Exception(str(e))
